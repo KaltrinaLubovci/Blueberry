611b61d60567fff673d4b1e3a4af609b2ec33014
erindamurseli <erindamurseli@outlook.com>
2020-06-13 22:51:49 +0200

    Add: Register user Database - Sign Up Screen.

--- app/src/main/java/com/kl/blueberry/ui/sign_up/SignUpViewModel.java
+++ app/src/main/java/com/kl/blueberry/ui/sign_up/SignUpViewModel.java
@@ -1,7 +1,40 @@
 package com.kl.blueberry.ui.sign_up;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.lifecycle.ViewModel;
+
+import com.kl.blueberry.data.Database;
+import com.kl.blueberry.model.register_user.User;
+
 /**
  * Created by Kaltrina Lubovci on 09,June,2020
  */
-public class SignUpViewModel {
+public class SignUpViewModel extends ViewModel {
+
+    public void registerUser(Context context, String fullName, String username, String email, String password) {
+        ContentValues cv = new ContentValues();
+        cv.put(User.Fullname, fullName);
+        cv.put(User.Username, username);
+        cv.put(User.Email, email);
+        cv.put(User.Password, password);
+
+        SQLiteDatabase objDb = new Database(context).getWritableDatabase();
+
+        try {
+            long retValue = objDb.insert(Database.UserTable, null, cv);
+            if (retValue > 0) {
+                Toast.makeText(context, "ID inserted: " + retValue,
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception ex) {
+            Log.e("excep", ex.getMessage());
+        } finally {
+            objDb.close();
+        }
+    }
 }
